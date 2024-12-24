633ae5838c27c729cf66fce74863169b31721421
Adrian <bontojohnadrian@gmail.com>
2024-12-24 13:43:00 +0800

    finish setting up the mailtrap.config.js

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -1,11 +1,17 @@
 import dotenv from "dotenv";
+import { MailtrapClient } from "mailtrap";
+import nodemailer from "nodemailer";
+
+// Load environment variables from .env file
 dotenv.config();
 
-var transport = nodemailer.createTransport({
-  host: process.env.MAILTRAP_HOST,
-  port: 587,
-  auth: {
-    user: "api",
-    pass: process.env.MAILTRAP_PASSWORD,
-  },
+export const mailtrapClient = new MailtrapClient({
+  endpoint: process.env.MAILTRAP_HOST,
+  token: process.env.MAILTRAP_API_TOKEN,
 });
+
+// sender
+export const sender = {
+  email: "mailtrap@demomailtrap.com",
+  name: "John Adrian Bonto",
+};
