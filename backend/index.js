cb45bc211f7d7fde957175595a60b5360facea80
Adrian <bontojohnadrian@gmail.com>
2024-12-31 23:57:03 +0800

    setting up the front end

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json()); // allow us to parse incoming json data
 app.use(cookieParser());
 // run the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}...`);
+  console.log(`-BACKEND SERVER RUNNING' ON PORT: ${PORT}...`);
 });
 
 // verify node env
