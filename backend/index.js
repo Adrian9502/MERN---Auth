28345d0d50e90793a40fc191fc222405203af213
Adrian <bontojohnadrian@gmail.com>
2025-01-05 11:10:41 +0800

    implemented signup functionality and added cors in backend server

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,14 @@ import connectDB from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-
+import cors from "cors";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 // middleware
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json()); // allow us to parse incoming json data
 app.use(cookieParser());
 // run the server
