fe61e92b3692d8dfe6caf0bf568ba28fbe6d9a22
Adrian <bontojohnadrian@gmail.com>
2025-01-09 16:31:20 +0800

    fix db connection error

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,11 +1,20 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+const uri = process.env.MONGO_URI; // Make sure this matches the variable in your .env file
+if (!uri) {
+  console.error("MONGO_URI is not defined in .env file");
+  process.exit(1); // Exit if no connection string is found
+}
 // MongoDB connection
 export default async function connectDB() {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log("CONNECTED TO MONGODB: ", conn.connection.host);
   } catch (error) {
+    console.log();
+
     console.error("NOT CONNECTED TO MONGODB:", error.message);
     process.exit(1); // 1 is for exit with failure, 0 is for success
   }
--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 const __dirname = path.resolve();
 
 // Connect to database
